77c63bc98af53cb853f072548a111b32dd46bb9b
radian462 <kumita435675@gmail.com>
2024-10-13 22:57:43 +0900

    軽くメインページを作成

--- main.py
+++ main.py
@@ -15,34 +15,49 @@ class option:
     filetime_index: int = 0
 
 
-
 def main(page: ft.Page):
+    class on_change_class:
+        def textfield(self, var):
+            def set_value(e):
+                setattr(op, var, e.control.value)
+            return set_value
+
+        def tab(self, var):
+            def set_value(e):
+                setattr(op, var, e.control.selected_index)
+            return set_value
+
     def main_page():
         url_tf = ft.TextField(
             label="URL",
             border="underline",
             prefix_icon=ft.icons.LINK,
             value=op.url,
-            on_change=on_change("url"),
+            on_change=lambda _: on_change.textfield("url"),
             expand=True,
         )
 
         download_b = ft.TextButton(
             "ダウンロード", 
             icon="download", 
-            on_click=lambda _: print(op.url)
+            on_click=lambda _: page.go("/download")
         )
 
         format_tab = ft.Tabs(
             selected_index=op.format_index,
-            on_change=tab_on_change("format_index"),
+            on_change=on_change.tab("format_index"),
             tabs=[
                 ft.Tab(text="動画"),
                 ft.Tab(icon=ft.icons.VOLUME_OFF, text="動画"),
                 ft.Tab(text="音声"),
             ],
         )
-        
+
+        setting_b = ft.TextButton(
+            "設定", 
+            icon=ft.icons.SETTINGS, 
+            on_click=lambda _: page.go("/settings"),
+        )
 
         page.views.append(
             ft.View(
@@ -50,9 +65,17 @@ def main(page: ft.Page):
                 [
                     ft.Column(
                         [
-                            ft.Container(
-                                ft.Row([url_tf, download_b]),
-                            )
+                            ft.Row([url_tf, download_b]),
+                            ft.Row(
+                                [
+                                    format_tab, 
+                                    ft.Container(
+                                        setting_b,
+                                        alignment=ft.alignment.center_right,
+                                        expand=True,
+                                    ),
+                                ]
+                            ),
                         ]
                     )
                 ]
@@ -65,18 +88,8 @@ def main(page: ft.Page):
             main_page()
         page.update()
 
-    def on_change(var):
-        def set_value(e):
-            setattr(op, var, e.control.value)
-        return set_value
-
-    def tab_on_change(var):
-        def set_value(e):
-            setattr(op, var, e.control.selected_index)
-        return set_value
-
-
     op = option()
+    on_change = on_change_class()
     page.title = "Flector"
     page.on_route_change = route_change
     page.go(page.route)
