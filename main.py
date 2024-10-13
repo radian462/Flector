cc41e2b83dc55aa79074cdc8301caa6394e94f28
radian462 <kumita435675@gmail.com>
2024-10-13 18:34:55 +0900

    urlとダウンロード追加

--- main.py
+++ main.py
@@ -23,12 +23,39 @@ def main(page: ft.Page):
             border="underline",
             prefix_icon=ft.icons.LINK,
             value=op.url,
+            on_change=on_change("url"),
             expand=True,
         )
+
+        download_b = ft.TextButton(
+            "ダウンロード", 
+            icon="download", 
+            on_click=lambda _: print(op.url)
+        )
+
+        format_tab = ft.Tabs(
+            selected_index=op.format_index,
+            on_change=tab_on_change("format_index"),
+            tabs=[
+                ft.Tab(text="動画"),
+                ft.Tab(icon=ft.icons.VOLUME_OFF, text="動画"),
+                ft.Tab(text="音声"),
+            ],
+        )
+        
+
         page.views.append(
             ft.View(
                 "/",
-                [url_tf]
+                [
+                    ft.Column(
+                        [
+                            ft.Container(
+                                ft.Row([url_tf, download_b]),
+                            )
+                        ]
+                    )
+                ]
             )
         )
     
@@ -37,7 +64,18 @@ def main(page: ft.Page):
         if page.route == "/":
             main_page()
         page.update()
-        
+
+    def on_change(var):
+        def set_value(e):
+            setattr(op, var, e.control.value)
+        return set_value
+
+    def tab_on_change(var):
+        def set_value(e):
+            setattr(op, var, e.control.selected_index)
+        return set_value
+
+
     op = option()
     page.title = "Flector"
     page.on_route_change = route_change
