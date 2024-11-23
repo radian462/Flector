ee2bfe6b884cc7592379786284706d635f27da7f
radian462 <kumita435675@gmail.com>
2024-11-23 20:12:01 +0900

    hookの作成(未デバッグ)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from dataclasses import dataclass
-import flet as ft
 import os
+import re
+
+import flet as ft
 from youtube_dl import youtube_dl, dd
 
 
@@ -15,26 +16,27 @@ class option:
         self.overwrite_index = 0
         self.filetime_index = 0
 
-    def textfield(self, var):
-        def set_value(e):
-            setattr(self, var, e.control.value)
-
-        return set_value
-
-    def tab(self, var):
-        def set_value(e):
-            setattr(self, var, e.control.selected_index)
-        return set_value
-
 
 def main(page: ft.Page):
+    def text_change(var):
+        def set_value(e):
+            setattr(op, var, e.control.value)
+
+        return set_value
+
+    def tab_change(var):
+        def set_value(e):
+            setattr(op, var, e.control.selected_index)
+
+        return set_value
+
     def main_page():
         url_tf = ft.TextField(
             label="URL",
             border="underline",
             prefix_icon=ft.icons.LINK,
             value=op.url,
-            on_change=lambda _: op.textfield("url"),
+            on_change=lambda _: text_change("url"),
             expand=True,
         )
 
@@ -44,7 +46,7 @@ def main(page: ft.Page):
 
         format_tab = ft.Tabs(
             selected_index=op.format_index,
-            on_change=op.tab("format_index"),
+            on_change=tab_change("format_index"),
             tabs=[
                 ft.Tab(text="動画"),
                 ft.Tab(icon=ft.icons.VOLUME_OFF, text="動画"),
@@ -97,40 +99,40 @@ def main(page: ft.Page):
 
         ext_list = ["mp3", "wav", "m4a", "aac", "flac"]
         ext_tab = ft.Tabs(
-            on_change=op.tab("ext_index"),
+            on_change=tab_change("ext_index"),
             selected_index=op.ext_index,
             tabs=[ft.Tab(text=e) for e in ext_list],
         )
 
         quality_list = ["240p", "360p", "480p", "720p", "1080p", "最高画質"]
         quality_tab = ft.Tabs(
-            on_change=op.tab("quality_index"),
+            on_change=tab_change("quality_index"),
             selected_index=op.quality_index,
             tabs=[ft.Tab(text=e) for e in quality_list],
         )
 
         title_tf = ft.TextField(
             value=op.title,
-            on_change=op.textfield("title"),
+            on_change=text_change("title"),
             border="underline",
         )
 
         storage_tf = ft.TextField(
             value=op.storage,
-            on_change=op.textfield("storage"),
+            on_change=text_change("storage"),
             border="underline",
         )
 
         overwrite_list = ["True", "False"]
         overwrite_tab = ft.Tabs(
-            on_change=op.tab("overwrite_index"),
+            on_change=tab_change("overwrite_index"),
             selected_index=op.overwrite_index,
             tabs=[ft.Tab(text=e) for e in overwrite_list],
         )
 
         filetime_list = ["現在時刻", "投稿時刻"]
         filetime_tab = ft.Tabs(
-            on_change=op.tab("filetime_index"),
+            on_change=tab_change("filetime_index"),
             selected_index=op.filetime_index,
             tabs=[ft.Tab(text=e) for e in filetime_list],
         )
@@ -161,8 +163,8 @@ def main(page: ft.Page):
                                                     e["text"],
                                                     size=20,
                                                     weight=ft.FontWeight.BOLD,
-                                                    ),
-                                                    e["widget"],
+                                                ),
+                                                e["widget"],
                                             ]
                                         ),
                                         margin=ft.margin.only(top=30 if i != 0 else 0),
@@ -171,16 +173,46 @@ def main(page: ft.Page):
                                 ]
                             ),
                         ],
-                        scroll=ft.ScrollMode.ALWAYS, 
-                        expand=True
+                        scroll=ft.ScrollMode.ALWAYS,
+                        expand=True,
                     )
                 ],
             )
         )
-    
+
     def download_page():
+        download_pb = ft.ProgressBar(width=page.window.width * 0.9, value=0)
+        dl_display = ft.Text(value="動画情報を取得中です", size=20)
+
+        page.views.append(
+            ft.View(
+                "/download",
+                controls=[
+                    ft.Container(
+                        ft.Column(
+                            [dl_display, download_pb],
+                            alignment=ft.MainAxisAlignment.CENTER,
+                            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                        ),
+                        width=page.window.width,
+                        height=page.window.height,
+                    )
+                ],
+            )
+        )
+
         def progress_hook(d):
-            pass
+            if d["status"] == "downloading":
+                percent = float(
+                    re.sub(r"\x1b\[[0-9;]*m", "", d["_percent_str"])
+                    .strip()
+                    .replace("%", "")
+                )
+                download_pb.value = percent * 0.01
+                page.update()
+
+        now_format = 1
+        format_count = 2 if op.format_index == 0 else 1
 
         format_dict = {
             "format": ["movie", "movie_mute", "audio"],
